5aa5458f84d7cb3ddee3ef499a72353cf27aec18
Serena Khoo <klingmin@rosetta15.csail.mit.edu>
2023-11-18 18:48:03 -0500

    Added in prompting file

---
+++ prompting_for_confidence.py
@@ -0,0 +1,12 @@
+import os 
+
+
+from dataloaders import get_dataloader
+
+
+if __name__ == "__main__":
+
+    truthfulqa_train_dataloader = get_dataloader("truthfulqa", )
+
+    print("okay i am here")
+    
